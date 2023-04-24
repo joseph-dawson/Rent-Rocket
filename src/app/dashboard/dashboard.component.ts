86e90c6164317de84d67a4f41f21463723930c4e
jpdaws2003 <jdaws2003@gmail.com>
2023-04-24 02:55:21 -0500

    Formatted dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  imagePath: string;
 
+  constructor() {
+    this.imagePath = '/assets/img/Welcome.png'
+  }
 }
