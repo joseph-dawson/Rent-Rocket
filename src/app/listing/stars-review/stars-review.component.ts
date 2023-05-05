c2cc1275114cb95853aea82ba636aa78d647b8c9
ParkerKalthoff <parkerlkalthoff@gmail.com>
2023-05-04 23:58:04 -0500

    starz dawg straight up

--- src/app/listing/stars-review/stars-review.component.ts
+++ src/app/listing/stars-review/stars-review.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { Property } from '../../property-service/property';
+import { PropertyService } from '../../property-service/property.service';
+import { Review } from '../../review-service/review';
+import { ReviewService } from '../../review-service/review.service';
+import { Landlord } from '../../landlord-service/landlord';
+import { LandlordService } from '../../landlord-service/landlord.service';
+import { SearchComponent } from '../../search/search.component';
+
 @Component({
   selector: 'app-stars-review',
   templateUrl: './stars-review.component.html',
   styleUrls: ['./stars-review.component.css']
 })
 export class StarsReviewComponent {
+  
+  htmlStar(rating: number): void {
 
+    const fullStar = '<span class="fa fa-star checked"></span>';
+    const emptyStar = '<span class="fa fa-star"></span>';
+    let carryover = 0;
+    let stars = '';
+
+    for (let i = rating; i > 0; i--) {
+      stars += fullStar;
+        carryover += 1; 
+      }
+
+    carryover = 5 - carryover;
+
+    for (let i = carryover; i > 0; i-- ) {
+        stars += emptyStar; 
+      }
+    }
 }
